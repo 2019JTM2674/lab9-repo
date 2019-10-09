4cdb1dfae2df54c60757ac0a217554295b9001b7
2019JTM2674 <jtm192674@iitd.ac.in>
2019-10-09 12:37:02 +0530

    ps1 changes

--- ps1.py
+++ ps1.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
+
+#Creation of class Author
 class Author:
-    def __init__(self,name,email,gender):
+    def __init__(self,name,email,gender):   #initialising function
         self.name=name
         self.email=email
         self.gender=gender
         
-    def get_name(self):
+    def get_name(self):                     #Getter for name
         return self.name
     
-    def get_geder(self):
+    def get_geder(self):                    #Getter for gender
         return self.gender
     
-    def set_email(self,new_email):
+    def set_email(self,new_email):          #Setter for email
         self.email=new_email
     
-    def get_email(self):
+    def get_email(self):                    #Getter for email
         return self.email
         
-    def printinfo(self):
-        #print(self.name,"(",self.gender,") at",self.email)
+    def printinfo(self):                    #print function
         print("{} ({}) at {}".format(self.name,self.gender,self.email))
     
     
     
     
 
-
+#creating instance of Author object
 person1=Author("Krishna","kc@gmail.com","m")
 person1.printinfo()
